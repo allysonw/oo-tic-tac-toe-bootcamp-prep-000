e5ce340fe35e8d3fad060b333824a3cc1024c82c
Allyson Wesman <allysonwesman@gmail.com>
2018-01-06 00:25:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -115,4 +115,17 @@ class TicTacToe
       return nil
     end
   end
-end
+
+  def play
+    until over?
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
+
+end # end TicTacToe Class
